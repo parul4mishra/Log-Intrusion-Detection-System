330ab66c7e9ae73b2495d88fab0bde9264fbc15a
Parul Mishra <69690369+parul4mishra@users.noreply.github.com>
2026-01-15 11:13:32 +0530

    Add loggers for debugging

--- log-alert-system/src/main/java/com/devbytes/alertsystem/ingestion/FileLogSource.java
+++ log-alert-system/src/main/java/com/devbytes/alertsystem/ingestion/FileLogSource.java
@@ -6,23 +6,37 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
 public class FileLogSource implements LogSource {
+	
+	private static final Logger log = LoggerFactory.getLogger(FileLogSource.class);
 
-	 private final Path logDir = Paths.get("/logs");
+	 //private final Path logDir = Paths.get("/logs");
+	private final Path logDir = Paths.get("logs");
 
-	    @Override
-	    public Stream<String> readLines() throws IOException {
-	    	if (!Files.exists(logDir)) 
-	    		return Stream.empty();
-	    	
-	    	return Files.list(logDir).flatMap(path -> {
-                try {
-                    return Files.lines(path);
-                } catch (IOException e) {
-                    //throw new RuntimeException("Failed to read log file: " + path, e);
-                	return Stream.empty();
-                }
-            		
+
+		@Override
+		public Stream<String> readLines() throws IOException {
+
+			log.info("log.ingestion.start directory={}", logDir);
+
+			if (!Files.exists(logDir)) {
+				log.warn("log.ingestion.dir_missing dir={}", logDir);
+				return Stream.empty();
+			}
+
+			return Files.list(logDir).flatMap(path -> {
+				try {
+					log.info("log.ingestion.file_start file={}", path.getFileName());
+					return Files.lines(path);
+				} catch (IOException e) {
+					log.error("log.ingestion.file_failed file={} error={}", path.getFileName(), e.getMessage(), e);
+					return Stream.empty();
+				}
     	
     	
     });
