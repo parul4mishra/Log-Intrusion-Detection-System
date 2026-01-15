f3cff6b206fb6fd5ed6042fdc5c33e4e1aee4eec
Parul Mishra <69690369+parul4mishra@users.noreply.github.com>
2026-01-15 11:17:29 +0530

    Add loggers and trace for log events

--- log-alert-system/src/main/java/com/devbytes/alertsystem/detection/DetectionRules.java
+++ log-alert-system/src/main/java/com/devbytes/alertsystem/detection/DetectionRules.java
@@ -1,12 +1,9 @@
 package com.devbytes.alertsystem.detection;
 
-import java.util.Optional;
-
-import com.devbytes.alertsystem.model.AlertEvent;
 import com.devbytes.alertsystem.model.LogEvent;
 
 public interface DetectionRules {
 	
-	Optional<AlertEvent> evaluate(LogEvent event);
+	void evaluate(LogEvent event);
 
 }
