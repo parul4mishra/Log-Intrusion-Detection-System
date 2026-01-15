be9ecb552bcb286cdc0359fda6d64f75660cb050
Parul Mishra <69690369+parul4mishra@users.noreply.github.com>
2026-01-15 11:11:53 +0530

    Add eventlisterner to automate reading logs

--- log-alert-system/src/main/java/com/devbytes/alertsystem/service/DefaultLogProcessorService.java
+++ log-alert-system/src/main/java/com/devbytes/alertsystem/service/DefaultLogProcessorService.java
@@ -2,6 +2,10 @@ package com.devbytes.alertsystem.service;
 
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 import com.devbytes.alertsystem.detection.DetectionService;
@@ -11,21 +15,20 @@ import com.devbytes.alertsystem.parser.LogParser;
 
 @Component
 public class DefaultLogProcessorService implements LogProcessingService {
+	
+	private static final Logger log = LoggerFactory.getLogger(DefaultLogProcessorService.class);
 
 	private final LogSource logSource;
     private final LogParser logParser;
     private final DetectionService detectionService;
 
-    public DefaultLogProcessorService(
-            LogSource logSource,
-            LogParser logParser,
-            DetectionService detectionService
-    ) {
-        this.logSource = logSource;
-        this.logParser = logParser;
-        this.detectionService = detectionService;
-    }
+	public DefaultLogProcessorService(LogSource logSource, LogParser logParser, DetectionService detectionService) {
+		this.logSource = logSource;
+		this.logParser = logParser;
+		this.detectionService = detectionService;
+	}
 
+	@EventListener(ApplicationReadyEvent.class)
     @Override
     public void processLogs() throws IOException {
         logSource.readLines()
@@ -33,18 +36,25 @@ public class DefaultLogProcessorService implements LogProcessingService {
     }
 
     private void processLine(String rawLine) {
+           
         try {
-            if (!logParser.supports(rawLine)) {
-                return; // skip unsupported format
+        	rawLine = rawLine.replace("\uFEFF", "");
+            if (!logParser.isParseble(rawLine)) {
+            	log.debug("log.processor.line.skipped raw='{}' length={}", rawLine, rawLine.length());
+                return;
             }
 
             LogEvent event = logParser.parse(rawLine);
+            String traceId = event.getAttributes().get("traceId");
             detectionService.applyRules(event);
 
         } catch (Exception e) {
-            // Fail-safe: never break the pipeline
-            // log and move on
-            System.err.println("Failed to process log line: " + rawLine);
+        	log.error(
+                    "log.processor.line.failed rawLine='{}' error={}",
+                    rawLine,
+                    e.getMessage(),
+                    e
+                );
         }
     }
 	
